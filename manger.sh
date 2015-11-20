322ddc904363e2d836dc250c53527095da4c45f3
petitdej <jb.warloe@gmail.com>
2015-11-20 12:26:22 +0100

    prise en compte des OS

--- manger.sh
+++ manger.sh
@@ -1,2 +1,18 @@
+#!/bin/sh
 echo "bon appetit!"
-open valtech.jpg
+export $OS="uname"
+echo "OS is "$OS"."
+case "$OS" in
+		"Windows_NT")               
+				cygstart "valtech.jpg";;
+        "CYGWIN_NT-6.1")               
+				start "valtech.jpg";;
+        "Linux")
+                start "valtech.jpg";;
+		"Darwin")
+                open "valtech.jpg";;
+        "*")
+                echo "Exiting. The OS type is not found.";;
+esac
+echo "Done."
+exit 0
